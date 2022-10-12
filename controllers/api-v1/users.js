545030972f9d7cf5bbfcd1e1c97fd4564921f8c7
Shazammon <ammonorgill@gmail.com>
2022-10-12 11:41:45 -0600

    merge conflict resolve

--- controllers/api-v1/users.js
+++ controllers/api-v1/users.js
@@ -160,21 +160,6 @@ router.post("/goals", authLockedRoute, async(req,res) => {
   }
 })
 
-// GET /users/goals/:goalId -- display a goal
-router.get("/goals/:goalId", authLockedRoute, async(req,res) => {
-  try {
-    const oneGoal = await db.User.findOne({
-          _id: res.locals.user._id, "goals._id": req.params.goalId
-      })
-  
-          res.json(oneGoal)
-  
-      } catch(err) {
-      console.log(err)
-      return res.status(500).json({error: "Server Error"})        
-  }
-})
-
 // PUT /users/goals/:goalId -- update a goal
 router.put("/goals/:goalId", authLockedRoute, async(req,res) => {
   try {
@@ -216,7 +201,6 @@ router.put("/goals/:goalId/status", authLockedRoute, async(req,res) => {
   }
 })
 
-// DELETE /goals/:goalId -- delete specific goal subdoc
 router.delete("/goals/:goalId", authLockedRoute, async(req,res) => {
   try {
     // console.log(res.locals.user)
